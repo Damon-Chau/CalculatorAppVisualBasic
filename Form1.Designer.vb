9f5109308207979e1c2d614f0fcee122eced6ad8
072665995 <chau413@gmail.com>
2015-10-15 20:54:19 -0400

    Finished  code
    simple calculator that's functional

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,9 +22,141 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.subtractRB = New System.Windows.Forms.RadioButton()
+        Me.multiplyRB = New System.Windows.Forms.RadioButton()
+        Me.divideRB = New System.Windows.Forms.RadioButton()
+        Me.addRB = New System.Windows.Forms.RadioButton()
+        Me.userinput1Textbox = New System.Windows.Forms.TextBox()
+        Me.userinput2Textbox = New System.Windows.Forms.TextBox()
+        Me.calculateBtn = New System.Windows.Forms.Button()
+        Me.clearBtn = New System.Windows.Forms.Button()
+        Me.symbolLbl = New System.Windows.Forms.Label()
+        Me.answerLbl = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'subtractRB
+        '
+        Me.subtractRB.AutoSize = True
+        Me.subtractRB.Location = New System.Drawing.Point(3, 95)
+        Me.subtractRB.Name = "subtractRB"
+        Me.subtractRB.Size = New System.Drawing.Size(65, 17)
+        Me.subtractRB.TabIndex = 1
+        Me.subtractRB.TabStop = True
+        Me.subtractRB.Text = "Subtract"
+        Me.subtractRB.UseVisualStyleBackColor = True
+        '
+        'multiplyRB
+        '
+        Me.multiplyRB.AutoSize = True
+        Me.multiplyRB.Location = New System.Drawing.Point(82, 72)
+        Me.multiplyRB.Name = "multiplyRB"
+        Me.multiplyRB.Size = New System.Drawing.Size(60, 17)
+        Me.multiplyRB.TabIndex = 2
+        Me.multiplyRB.TabStop = True
+        Me.multiplyRB.Text = "Multiply"
+        Me.multiplyRB.UseVisualStyleBackColor = True
+        '
+        'divideRB
+        '
+        Me.divideRB.AutoSize = True
+        Me.divideRB.Location = New System.Drawing.Point(82, 95)
+        Me.divideRB.Name = "divideRB"
+        Me.divideRB.Size = New System.Drawing.Size(55, 17)
+        Me.divideRB.TabIndex = 3
+        Me.divideRB.TabStop = True
+        Me.divideRB.Text = "Divide"
+        Me.divideRB.UseVisualStyleBackColor = True
+        '
+        'addRB
+        '
+        Me.addRB.AutoSize = True
+        Me.addRB.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.addRB.Location = New System.Drawing.Point(3, 72)
+        Me.addRB.Name = "addRB"
+        Me.addRB.Size = New System.Drawing.Size(44, 17)
+        Me.addRB.TabIndex = 0
+        Me.addRB.TabStop = True
+        Me.addRB.Text = "Add"
+        Me.addRB.UseVisualStyleBackColor = False
+        '
+        'userinput1Textbox
+        '
+        Me.userinput1Textbox.Location = New System.Drawing.Point(3, 13)
+        Me.userinput1Textbox.Name = "userinput1Textbox"
+        Me.userinput1Textbox.Size = New System.Drawing.Size(269, 20)
+        Me.userinput1Textbox.TabIndex = 4
+        '
+        'userinput2Textbox
+        '
+        Me.userinput2Textbox.Location = New System.Drawing.Point(3, 28)
+        Me.userinput2Textbox.Name = "userinput2Textbox"
+        Me.userinput2Textbox.Size = New System.Drawing.Size(269, 20)
+        Me.userinput2Textbox.TabIndex = 5
+        '
+        'calculateBtn
+        '
+        Me.calculateBtn.Location = New System.Drawing.Point(143, 72)
+        Me.calculateBtn.Name = "calculateBtn"
+        Me.calculateBtn.Size = New System.Drawing.Size(75, 23)
+        Me.calculateBtn.TabIndex = 6
+        Me.calculateBtn.Text = "Calculate"
+        Me.calculateBtn.UseVisualStyleBackColor = True
+        '
+        'clearBtn
+        '
+        Me.clearBtn.Location = New System.Drawing.Point(143, 101)
+        Me.clearBtn.Name = "clearBtn"
+        Me.clearBtn.Size = New System.Drawing.Size(75, 23)
+        Me.clearBtn.TabIndex = 7
+        Me.clearBtn.Text = "Clear"
+        Me.clearBtn.UseVisualStyleBackColor = True
+        '
+        'symbolLbl
+        '
+        Me.symbolLbl.AutoSize = True
+        Me.symbolLbl.Location = New System.Drawing.Point(0, 51)
+        Me.symbolLbl.Name = "symbolLbl"
+        Me.symbolLbl.Size = New System.Drawing.Size(0, 13)
+        Me.symbolLbl.TabIndex = 9
+        '
+        'answerLbl
+        '
+        Me.answerLbl.AutoSize = True
+        Me.answerLbl.Location = New System.Drawing.Point(210, 51)
+        Me.answerLbl.Name = "answerLbl"
+        Me.answerLbl.Size = New System.Drawing.Size(0, 13)
+        Me.answerLbl.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.answerLbl)
+        Me.Controls.Add(Me.symbolLbl)
+        Me.Controls.Add(Me.clearBtn)
+        Me.Controls.Add(Me.calculateBtn)
+        Me.Controls.Add(Me.userinput2Textbox)
+        Me.Controls.Add(Me.userinput1Textbox)
+        Me.Controls.Add(Me.divideRB)
+        Me.Controls.Add(Me.multiplyRB)
+        Me.Controls.Add(Me.subtractRB)
+        Me.Controls.Add(Me.addRB)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents addRB As RadioButton
+    Friend WithEvents subtractRB As RadioButton
+    Friend WithEvents multiplyRB As RadioButton
+    Friend WithEvents divideRB As RadioButton
+    Friend WithEvents userinput1Textbox As TextBox
+    Friend WithEvents userinput2Textbox As TextBox
+    Friend WithEvents calculateBtn As Button
+    Friend WithEvents clearBtn As Button
+    Friend WithEvents symbolLbl As Label
+    Friend WithEvents answerLbl As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,93 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub additionRb_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles addRB.CheckedChanged
+        symbolLbl.Text = "+"
+        answerLbl.Text = ""
+    End Sub
+
+    Private Sub subtractRb_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles subtractRB.CheckedChanged
+        symbolLbl.Text = "-"
+        answerLbl.Text = ""
+    End Sub
+
+    Private Sub multiplyRb_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles multiplyRB.CheckedChanged
+        symbolLbl.Text = "*"
+        answerLbl.Text = ""
+    End Sub
+
+    Private Sub divideRb_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles divideRB.CheckedChanged
+        symbolLbl.Text = "/"
+        answerLbl.Text = ""
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles symbolLbl.Click
+
+    End Sub
+
+    Private Sub answerLbl_Click(sender As Object, e As EventArgs) Handles answerLbl.Click
+
+    End Sub
+
+    Private Sub calculateBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles calculateBtn.Click
+        Dim number1 As Double
+        Dim number2 As Double
+        Dim answer As Double
+
+        If userinput1Textbox.Text = "" OrElse userinput2Textbox.Text = "" Then
+            MessageBox.Show("Please Enter a Number")
+        ElseIf addRb.Checked = False And subtractRB.Checked = False And multiplyRB.Checked = False And divideRB.Checked = False Then
+            MessageBox.Show("Please Choose an Operation")
+        Else
+            number1 = Double.Parse(userinput1Textbox.Text)
+            number2 = Double.Parse(userinput2Textbox.Text)
+
+            If addRB.Checked = True Then
+                answer = number1 + number2
+                answerLbl.Text = answer.ToString()
+            ElseIf subtractRB.Checked = True Then
+                answer = number1 - number2
+                answerLbl.Text = answer.ToString()
+            ElseIf multiplyRB.Checked = True Then
+                If number1 = 0 OrElse number2 = 0 Then
+                    answerLbl.Text = "0"
+                Else
+                    answer = number1 * number2
+                    answerLbl.Text = answer.ToString()
+                End If
+            ElseIf divideRB.Checked = True Then
+                If number1 = 0 Then
+                    answerLbl.Text = "0"
+                ElseIf number2 = 0 Then
+                    answerLbl.Text = "Cannot Divide by Zero"
+                Else
+                    answer = number1 / number2
+                    answerLbl.Text = answer.ToString()
+                End If
+            End If
+        End If
+
+    End Sub
+
+    Private Sub clearBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles clearBtn.Click
+        userinput1Textbox.Text = ""
+        userinput2Textbox.Text = ""
+        answerLbl.Text = ""
+
+        If addRB.Checked = True Then
+            addRB.Checked = False
+            symbolLbl.Text = ""
+        ElseIf subtractRB.Checked = True Then
+            subtractRB.Checked = False
+            symbolLbl.Text = ""
+        ElseIf multiplyRB.Checked = True Then
+            multiplyRB.Checked = False
+            symbolLbl.Text = ""
+        Else
+            divideRB.Checked = False
+            symbolLbl.Text = ""
+        End If
+    End Sub
 End Class
